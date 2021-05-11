171f45393b55e3a88a2dcf2dca0ad4138d16c6a6
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2021-05-11 12:07:47 -0400

    draft

--- src/index.js
+++ src/index.js
@@ -14,14 +14,13 @@ class Game extends Phaser.Game {
 	constructor() {
 		super(config);
 		this.scene.add('Boot', BootScene);
-
-		this.scene.add('Puzzle1', Puzzle1);
+this.scene.add('Puzzle1', Puzzle1);
 		this.scene.add('Preloader', PreloaderScene);
 		this.scene.add('Title', TitleScene);
 		this.scene.add('Options', OptionsScene);
 		this.scene.add('Credits', CreditsScene);
 		this.scene.add('Game', GameScene);
-		this.scene.start('Game');
+		this.scene.start('Puzzle1');
 	}
 }
 
