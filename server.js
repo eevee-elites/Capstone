71f74ebb80ae280300eb03f8eb3795dd502d1cc8
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2021-05-11 16:26:43 -0400

    attempt to deploy with server.js file

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const DIST_DIR = path.join(__dirname, '/dist');
-const HTML_FILE = path.join(DIST_DIR, 'index.html');
+// const DIST_DIR = path.join(__dirname, '/dist');
+const HTML_FILE = path.join('index.html');
 
 app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 
