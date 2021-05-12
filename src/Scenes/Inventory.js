8f2ebc9440c2ace0fbd914aaf878a2ff98cfa916
Dinalisse Felicione <dfelicione@gmail.com>
2021-05-12 18:23:28 -0400

    popup inventory in the works

--- src/Scenes/Inventory.js
+++ src/Scenes/Inventory.js
@@ -13,27 +13,31 @@ export default class Inventory extends Phaser.Scene {
       url: "https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js",
       sceneKey: "rexUI",
     });
+    this.load.plugin(
+      "rexcirclemaskimageplugin",
+      "https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexcirclemaskimageplugin.min.js",
+      true
+    );
+
+    this.load.image("pizza", "assets/pizza.png");
   }
 
   create() {
-    this.print = this.add.text(0, 580, "press i to pop-up inventory");
-
+    createDialog.bind(this);
+    this.pizza = this.add.image(0, 0, "pizza");
     var scene = this,
       dialog = undefined;
     this.input.on(
       "pointerdown",
       function (pointer) {
-        var x = 300,
-          y = 500;
+        var x = pointer.x,
+          y = pointer.y;
 
         if (dialog === undefined) {
           dialog = createDialog(this, x, y, function (color) {
-            scene.add.circle(x, y, 20, color);
-            scene.print.text = "Add object at (" + x + "," + y + ")";
             dialog.scaleDownDestroy(100);
             dialog = undefined;
           });
-          scene.print.text = "Click (" + x + "," + y + ")";
         } else if (!dialog.isInTouching(pointer)) {
           dialog.scaleDownDestroy(100);
           dialog = undefined;
@@ -49,14 +53,14 @@ export default class Inventory extends Phaser.Scene {
 var createDialog = function (scene, x, y, onClick) {
   var dialog = scene.rexUI.add
     .dialog({
-      x: x,
-      y: y,
+      x: 350,
+      y: 0,
 
       background: scene.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0xf57f17),
 
       title: scene.rexUI.add.label({
         background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0xbc5100),
-        text: scene.add.text(0, 0, "Pick a color", {
+        text: scene.add.text(0, 0, "Inventory", {
           fontSize: "20px",
         }),
         space: {
@@ -72,9 +76,12 @@ var createDialog = function (scene, x, y, onClick) {
         scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x673ab7),
         scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x2196f3),
         scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x00bcd4),
-        scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x4caf50),
         scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0xcddc39),
+        scene.add.rexCircleMaskImage(0, 0, "assets/pizza.png"),
+
+        // scene.rexUI.add.image(0, 0, 0, 0, 20, "pizza"),
       ],
+      // icon: scene.add.image(0, 0, 0, 0, 20, "pizza"),
 
       actionsAlign: "left",
 
@@ -106,13 +113,3 @@ var createDialog = function (scene, x, y, onClick) {
 
   return dialog;
 };
-
-// var config = {
-//   type: Phaser.AUTO,
-//   parent: "phaser-example",
-//   width: 800,
-//   height: 600,
-//   scene: Demo,
-// };
-
-// var game = new Phaser.Game(config);
