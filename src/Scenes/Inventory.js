85dd16ab703d974ccd3cf989df1f0d3e04544b02
Dinalisse Felicione <dfelicione@gmail.com>
2021-05-14 11:31:31 -0400

    logic for switchin inventory screen working for tester

--- src/Scenes/Inventory.js
+++ src/Scenes/Inventory.js
@@ -1,114 +1,48 @@
-import 'phaser';
+import "phaser";
 
 export default class Inventory extends Phaser.Scene {
-	constructor() {
-		super({
-			key: 'Inventory',
-		});
-	}
+  constructor() {
+    super("Inventory");
+  }
 
-	preload() {
-		this.load.scenePlugin({
-			key: 'rexuiplugin',
-			url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
-			sceneKey: 'rexUI',
-		});
-		this.load.plugin(
-			'rexcirclemaskimageplugin',
-			'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexcirclemaskimageplugin.min.js',
-			true
-		);
-		this.load.image('cat', 'assets/cat.png');
-		this.load.image('star', 'assets/star.png');
-		this.load.image('pizza', 'assets/pizza.png');
-	}
+  initialize() {
+    Phaser.Scene.call(this, "Inventory");
+  }
 
-	create() {
-		var scene = this,
-			dialog = undefined;
-		this.input.keyboard.on(
-			'keydown-I',
-			function (pointer) {
-				var x = pointer.x,
-					y = pointer.y;
+  preload() {
+    this.load.image("cat", "assets/cat.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("pizza", "assets/pizza.png");
+  }
 
-				if (dialog === undefined) {
-					dialog = this.createDialog(this, x, y, function (color) {
-						dialog.scaleDownDestroy(100);
-						dialog = undefined;
-					});
-				} else if (!dialog.isInTouching(pointer)) {
-					dialog.scaleDownDestroy(100);
-					dialog = undefined;
-				}
-			},
-			this
-		);
-	}
-	createDialog(scene, x, y, onClick) {
-		var dialog = scene.rexUI.add
-			.dialog({
-				x: 350,
-				y: 0,
+  create() {
+    this.add.image(200, 200, "cat");
 
-				background: scene.rexUI.add.roundRectangle(
-					0,
-					0,
-					100,
-					100,
-					20,
-					0xf57f17
-				),
+    this.input.once(
+      Phaser.Input.Events.POINTER_DOWN,
+      function (event) {
+        this.scene.switch("Tester");
+      },
+      this
+    );
 
-				title: scene.rexUI.add.label({
-					background: scene.rexUI.add.roundRectangle(
-						0,
-						0,
-						100,
-						40,
-						20,
-						0xbc5100
-					),
-					text: scene.add.text(0, 0, 'Inventory', {
-						fontSize: '20px',
-					}),
-					space: {
-						left: 15,
-						right: 15,
-						top: 10,
-						bottom: 10,
-					},
-				}),
+    this.events.on(
+      Phaser.Scenes.Events.WAKE,
+      function () {
+        this.wake(this.input, this.scene);
+      },
+      this
+    );
+  }
+  wake(input, scene) {
+    input.once(
+      Phaser.Input.Events.POINTER_DOWN,
+      function (event) {
+        scene.switch("Tester");
+      },
+      this
+    );
+  }
 
-				actions: [
-					scene.add.image(0, 0, 'pizza'),
-					scene.add.image(0, 0, 'cat'),
-					scene.add.image(0, 0, 'star'),
-					// scene.rexUI.add.image( "pizza"),
-				],
-
-				actionsAlign: 'left',
-
-				space: {
-					title: 10,
-					action: 5,
-
-					left: 10,
-					right: 10,
-					top: 10,
-					bottom: 10,
-				},
-			})
-			.layout()
-			.pushIntoBounds()
-			.popUp(500);
-
-		dialog.on('button.click', function (button, groupName, index) {
-			onClick(button.fillColor);
-		});
-
-		return dialog;
-	}
-
-	update() {}
+  update() {}
 }
--- src/Scenes/TesterScene.js
+++ src/Scenes/TesterScene.js
@@ -1,106 +1,138 @@
-import 'phaser';
-import Player from '../Models/Player';
-import NPC from '../Models/NPC';
-import Animate from '../Models/Animate';
-import NPCAnimate from '../Models/NPCAnimate';
-import Inventory from '../Helper/Inventory';
+import "phaser";
+import Player from "../Models/Player";
+import NPC from "../Models/NPC";
+import Animate from "../Models/Animate";
+import NPCAnimate from "../Models/NPCAnimate";
+import Inventory from "../Helper/Inventory";
 
 export default class TesterScene extends Phaser.Scene {
-	constructor() {
-		super('Tester');
-	}
+  constructor() {
+    super("Tester");
+  }
+  initialize() {
+    function switchInventory() {
+      Phaser.Scene.call(this, "Inventory");
+    }
+  }
 
-	preload() {
-		this.load.scenePlugin(
-			'rexuiplugin',
-			'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
-			'rexUI',
-			'rexUI'
-		);
-		this.load.spritesheet('man', 'assets/man.png', {
-			frameWidth: 64,
-			frameHeight: 64,
-		});
-		this.load.spritesheet('pika', 'assets/pika.png', {
-			frameWidth: 31,
-			frameHeight: 31,
-		});
-		this.load.audio('bg', 'assets/bg.wav');
-		this.load.image('star', 'assets/star.png');
-		this.load.image('cat', 'assets/cat.png');
-		this.load.image('pizza', 'assets/pizza.png');
-	}
+  preload() {
+    this.load.scenePlugin(
+      "rexuiplugin",
+      "https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js",
+      "rexUI",
+      "rexUI"
+    );
+    this.load.spritesheet("man", "assets/man.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet("pika", "assets/pika.png", {
+      frameWidth: 31,
+      frameHeight: 31,
+    });
+    this.load.audio("bg", "assets/bg.wav");
+    this.load.image("star", "assets/star.png");
+    this.load.image("cat", "assets/cat.png");
+    this.load.image("pizza", "assets/pizza.png");
+  }
 
-	create() {
-		let hitBox = this.add.rectangle(100, 400, 40, 40, 0x000000);
-		this.man = this.physics.add.existing(new Player(this, 400, 300, 'man'));
-		this.pika = this.physics.add.existing(
-			new NPC(this, 100, 400, 'pika'),
-			true
-		);
-		this.stars = this.physics.add.sprite(100, 450, 'star');
-		this.pizza = this.physics.add.sprite(333, 250, 'pizza');
-		this.cat = this.physics.add.sprite(200, 350, 'cat');
-		var music = this.sound.add('bg', true);
-		music.setLoop(true);
-		music.play();
-		music.setVolume(0.3);
+  create() {
+    let hitBox = this.add.rectangle(100, 400, 40, 40, 0x000000);
+    this.man = this.physics.add.existing(new Player(this, 400, 300, "man"));
+    this.pika = this.physics.add.existing(
+      new NPC(this, 100, 400, "pika"),
+      true
+    );
+    this.stars = this.physics.add.sprite(100, 450, "star");
+    this.pizza = this.physics.add.sprite(333, 250, "pizza");
+    this.cat = this.physics.add.sprite(200, 350, "cat");
+    var music = this.sound.add("bg", true);
+    music.setLoop(true);
+    music.play();
+    music.setVolume(0.3);
 
-		this.pika.body.setSize(25, 25, true);
+    this.pika.body.setSize(25, 25, true);
 
-		// hitBox.setInteractive();
-		Animate(this, 'man', 4, 7, 8, 11, 12, 15, 0, 3, 0);
-		NPCAnimate(this, 'pika', 2, 3, 6, -1);
+    // hitBox.setInteractive();
+    Animate(this, "man", 4, 7, 8, 11, 12, 15, 0, 3, 0);
+    NPCAnimate(this, "pika", 2, 3, 6, -1);
 
-		let testBox = this.add.rectangle(100, 100, 100, 100, 0xffffff);
+    let testBox = this.add.rectangle(100, 100, 100, 100, 0xffffff);
 
-		this.man.setCollideWorldBounds(true);
+    this.man.setCollideWorldBounds(true);
 
-		//this adds collision to given object, and sets static to true so it can't be moved
-		this.physics.add.existing(testBox, true);
-		this.physics.add.existing(hitBox, true);
+    //this adds collision to given object, and sets static to true so it can't be moved
+    this.physics.add.existing(testBox, true);
+    this.physics.add.existing(hitBox, true);
 
-		this.physics.add.collider(testBox, this.man);
-		this.physics.add.collider(this.pika, this.man);
-		this.physics.add.overlap(this.man, this.stars, collectItem, null, this);
-		this.physics.add.overlap(this.man, this.pizza, collectItem, null, this);
-		this.physics.add.overlap(this.man, this.cat, collectItem, null, this);
+    this.physics.add.collider(testBox, this.man);
+    this.physics.add.collider(this.pika, this.man);
+    this.physics.add.overlap(this.man, this.stars, collectItem, null, this);
+    this.physics.add.overlap(this.man, this.pizza, collectItem, null, this);
+    this.physics.add.overlap(this.man, this.cat, collectItem, null, this);
 
-		var scene = this,
-			inventory = undefined;
-		this.input.keyboard.on(
-			'keydown-I',
-			function (pointer) {
-				var x = pointer.x,
-					y = pointer.y;
+    var scene = this,
+      inventory = undefined;
+    this.input.keyboard.on(
+      "keydown-I",
+      function (pointer) {
+        var x = pointer.x,
+          y = pointer.y;
 
-				if (inventory === undefined) {
-					inventory = Inventory(
-						this,
-						x,
-						y,
-						this.man.inventory,
-						function (color) {
-							inventory.scaleDownDestroy(100);
-							inventory = undefined;
-						}
-					);
-				} else if (!inventory.isInTouching(pointer)) {
-					inventory.scaleDownDestroy(100);
-					inventory = undefined;
-				}
-			},
-			this
-		);
-	}
+        if (inventory === undefined) {
+          inventory = Inventory(
+            this,
+            x,
+            y,
+            this.man.inventory,
+            function (color) {
+              inventory.scaleDownDestroy(100);
+              inventory = undefined;
+            }
+          );
+        } else if (!inventory.isInTouching(pointer)) {
+          inventory.scaleDownDestroy(100);
+          inventory = undefined;
+        }
+      },
+      this
+    );
+    /// scene switching logic
 
-	update() {
-		this.pika.update(this.pika, 'pika');
-		this.man.update(this);
-	}
+    this.input.once(
+      Phaser.Input.Events.POINTER_DOWN,
+      function () {
+        this.scene.switch("Inventory");
+      },
+      this
+    );
+
+    this.events.on(
+      Phaser.Scenes.Events.WAKE,
+      function () {
+        this.wake(this.input, this.scene);
+      },
+      this
+    );
+  }
+
+  wake(input, scene) {
+    input.once(
+      Phaser.Input.Events.POINTER_DOWN,
+      function () {
+        scene.switch("Inventory");
+      },
+      this
+    );
+  }
+
+  update() {
+    this.pika.update(this.pika, "pika");
+    this.man.update(this);
+  }
 }
 function collectItem(man, item) {
-	man.pickupItem(item.texture.key);
-	item.disableBody(true, true);
-	console.log(man.inventory);
+  man.pickupItem(item.texture.key);
+  item.disableBody(true, true);
+  console.log(man.inventory);
 }
