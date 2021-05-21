c2431c3a7ba60664d834092361be5826b6bb1976
Dinalisse Felicione <dfelicione@gmail.com>
2021-05-21 00:09:04 -0400

    puzzle1 strike system and gameOverScene incorporated

--- src/index.js
+++ src/index.js
@@ -16,29 +16,31 @@ import IntroScene from "./Scenes/IntroScene";
 import HallwayScene from "./Scenes/HallwayScene";
 import OutsideScene from "./Scenes/OutsideScene";
 import LobbyScene from "./Scenes/LobbyScene";
+import GameOverScene from "./Scenes/GameOverScene";
 
 class Game extends Phaser.Game {
-	constructor() {
-		super(config);
-		this.scene.add("Boot", BootScene);
-		this.scene.add("Puzzle1", Puzzle1);
-		this.scene.add("Preloader", PreloaderScene);
-		this.scene.add("Title", TitleScene);
-		this.scene.add("Options", OptionsScene);
-		this.scene.add("Credits", CreditsScene);
-		this.scene.add("StartScene", StartScene);
-		this.scene.add("Tester", TesterScene);
-		this.scene.add("Tester2", Tester2);
-		this.scene.add("Inventory", Inventory);
-		this.scene.add("Tutorial", TutorialScene);
-		this.scene.add("Intro", IntroScene);
-		this.scene.add("Hallway", HallwayScene);
-		this.scene.add("Outside", OutsideScene);
-		this.scene.add("Lobby", LobbyScene);
+  constructor() {
+    super(config);
+    this.scene.add("Boot", BootScene);
+    this.scene.add("Puzzle1", Puzzle1);
+    this.scene.add("Preloader", PreloaderScene);
+    this.scene.add("Title", TitleScene);
+    this.scene.add("Options", OptionsScene);
+    this.scene.add("Credits", CreditsScene);
+    this.scene.add("StartScene", StartScene);
+    this.scene.add("Tester", TesterScene);
+    this.scene.add("Tester2", Tester2);
+    this.scene.add("Inventory", Inventory);
+    this.scene.add("Tutorial", TutorialScene);
+    this.scene.add("Intro", IntroScene);
+    this.scene.add("Hallway", HallwayScene);
+    this.scene.add("Outside", OutsideScene);
+    this.scene.add("Lobby", LobbyScene);
     this.scene.add("Puzzle2", Puzzle2);
+    this.scene.add("GameOver", GameOverScene);
 
-		this.scene.start("Boot");
-	}
+    this.scene.start("Boot");
+  }
 }
 
 window.game = new Game();
