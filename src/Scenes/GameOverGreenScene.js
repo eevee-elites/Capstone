59d68f599ef1143079a5ab742f5f2990d42f4cf4
Dinalisse Felicione <dfelicione@gmail.com>
2021-05-24 21:23:51 -0400

    both gameOver scenes fixed

--- src/Scenes/GameOverGreenScene.js
+++ src/Scenes/GameOverGreenScene.js
@@ -11,7 +11,7 @@ export default class GameOverGreenScene extends Phaser.Scene {
   create() {
     this.add.image(400, 300, "GameoverGreen");
     this.add.text(550, 0, "Game Over!").setFontSize(30);
-    const restartButton = new Button(630, 800, "Restart Game", this, () => {
+    const restartButton = new Button(630, 80, "Restart Game", this, () => {
       this.scene.start("StartScene", {
         complete: { puzzle1: false, puzzle2: false },
       });
